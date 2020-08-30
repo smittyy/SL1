ed2fc5d1560cf5e55ccefe6d22c0a1e75dc69392
smittyy <smith_conner@student.mahoningctc.com>
2020-08-30 14:03:40 -0400

    Variables

---
+++ SoloLearn1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SoloLearn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var Bad_Name = "This is a bad Variable";
+            int number = 13;
+        }
+    }
+}
